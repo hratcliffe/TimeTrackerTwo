cc9f9734d8de56f2d3c35d8894e75497f91f2c6e
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-03-14 12:01:10 +0000

    Add missing function

--- include/dataInterface.h
+++ include/dataInterface.h
@@ -47,6 +47,7 @@ class dataIO{
 
     virtual timeStamp fetchTrackerAt(timecode time) = 0; /**< \brief Fetch the stamp 'active at' given time */
     virtual std::vector<timeStamp> fetchTrackerEntries(timecode start=-1, timecode end=-1) = 0; /**< \brief Fetch ORDERED tracker entries from the data source, optionally within a time range */
+    virtual std::vector<timeStamp> fetchTrackerEntries(proIds::Uuid const & id) = 0; /**< \brief Fetch ORDERED tracker entries for specific id */
     virtual timeStamp fetchLatestTrackerEntry() = 0;/**< \brief Fetch the latest (most recent) tracker entry */
 
     virtual void deleteTrackerInInterval(timecode start, timecode end) = 0;/**< \brief Delete tracker entries in the given range*/
@@ -172,6 +173,10 @@ class databaseIO : public dataIO{
       // Implementation for fetching tracker entries from database
       return dbStore.fetchTrackerEntries(start, end);
     }
+    std::vector<timeStamp> fetchTrackerEntries(proIds::Uuid const & id) override{
+       return dbStore.fetchTrackerEntries(id);
+    }
+
     timeStamp fetchLatestTrackerEntry() override{
       return dbStore.fetchLatestTrackerEntry();
     }

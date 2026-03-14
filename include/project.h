137e1dcd228392f5bd3aac3d89f6d94feb06aeaa
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-03-14 20:15:01 +0000

    BUGFIX - derived shadowing parent

--- include/project.h
+++ include/project.h
@@ -115,8 +115,6 @@ class project : public projectLike{
 
     bool active; /**< \brief Flag to allow project to be deactivated for any reason*/
     float FTE;/**< \brief Fraction of FTE for this project */
-    bool hasStart=false, hasEnd=false; /**< Whether or not there is a start or end */
-    timecode start=timecodeNull, end=timecodeNull; /**< Time for the start and end */
     std::vector<proIds::Uuid> subprojects;/**< \brief Subprojects belonging to this project */
   public:
     project() = default;

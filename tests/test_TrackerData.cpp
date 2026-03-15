92c843227a29a75b1c22fb1534be7ffe7b73012b
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-03-15 08:23:31 +0000

    Fix bug, add some tests
    Uninit argc -> segfaulting.
    Also adds some basic tests and convenience setup helper

--- tests/test_TrackerData.cpp
+++ tests/test_TrackerData.cpp
@@ -7,16 +7,24 @@
 // rest is too QT/Signal enmeshed
 
 auto dummyApp(){
-    int argc;
-    char** argv;
+    int argc=0;
+    char** argv=nullptr;
     return QApplication(argc, argv);
 }
+auto basicConfig(std::string file="./Scratch/TrackerDB.db"){
+  appConfig conf;
+  conf.backend = dataBackendType::database;
+  conf.dataFileName = file;
+  return conf;
+}
+
 TEST_CASE("Constructing a Tracker", "[QTAware]"){
   auto app = dummyApp();
   appConfig conf;
   conf.backend = dataBackendType::database;
   conf.dataFileName = "./Scratch/TrackerDB.db";
-  TrackerData td{conf};
+  auto init = [conf](){TrackerData td{conf};};
+  REQUIRE_NOTHROW(init());
 }
 
 TEST_CASE("Constructing a Tracker with bad backend", "[QTAware]"){
@@ -26,4 +34,22 @@ TEST_CASE("Constructing a Tracker with bad backend", "[QTAware]"){
   conf.dataFileName = "";
   auto init = [conf](){TrackerData td{conf};};
   REQUIRE_THROWS(init());
+}
+
+TEST_CASE("State round trip", "[QTAware]"){
+  auto app = dummyApp();
+  TrackerData td{basicConfig()};
+
+  td.writeState("abc3", 72);
+  int c = td.readState("abc3");
+  REQUIRE(c == 72);
+}
+
+TEST_CASE("Config round trip", "[QTAware]"){
+  auto app = dummyApp();
+  TrackerData td{basicConfig()};
+
+  td.writeConfig("vers", "Version c6qe");
+  std::string c = td.readConfig("vers");
+  REQUIRE(c == "Version c6qe");
 }

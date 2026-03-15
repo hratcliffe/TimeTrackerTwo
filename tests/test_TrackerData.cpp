56419528cda1c96eb55121de6bc934dfe6966f1a
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-03-15 16:13:13 +0000

    Cover loading

--- tests/test_TrackerData.cpp
+++ tests/test_TrackerData.cpp
@@ -384,7 +384,52 @@ TEST_CASE("Overall Summary", "[QTAware, Slots]"){
 // -------- Digest Generation ------------------------------------------------------------------------
 
 //----------- Loading Projects ----------------------------------------------------------------------
+TEST_CASE("Known Data - Load projects", "[QTAware]"){
+  auto app = dummyApp();
+  TrackerData td{basicConfig("./InputData/KnownDatabase.db")};
 
+  SignalCatcher sig;
+  QAbstractEventDispatcher::connect(&td, &TrackerData::projectListUpdateEvent, &sig, &SignalCatcher::emitOrderedProjectList);
+  QAbstractEventDispatcher::connect(&td, &TrackerData::projectTotalUpdateEvent, &sig, &SignalCatcher::emitDoubleX2);
+
+  td.loadProjects(0);
+
+  std::vector<selectableEntity> list;
+  list = sig.stashPayloadForReturn(list, false);
+  REQUIRE(list.size() == 5);
+  // Check for the five items
+  {auto check = [](selectableEntity & se){return se.name == "Project Alpha" && se.uid.to_string() == "{cc467402-acd5-494f-9c58-466f3aa6f117}" && se.level == 0;};
+  REQUIRE(std::find_if(list.begin(), list.end(), check) != list.end());}
+  {auto check = [](selectableEntity & se){return se.name == "Project Beta" && se.uid.to_string() == "{8af5d44a-2921-4666-b33b-053459e2ced6}" && se.level == 0;};
+  REQUIRE(std::find_if(list.begin(), list.end(), check) != list.end());}
+  //Subprojects:
+  {auto check = [](selectableEntity & se){return se.name == "Documentation" && se.uid.to_string() == "{6364fcb1-6a15-4b69-8412-7ef0eee6c94f}" && se.level == 1;};
+  REQUIRE(std::find_if(list.begin(), list.end(), check) != list.end());}
+  {auto check = [](selectableEntity & se){return se.name == "Testing" && se.uid.to_string() == "{de58a6f8-d0bb-46c8-af18-aed15e92060c}" && se.level == 1;};
+  REQUIRE(std::find_if(list.begin(), list.end(), check) != list.end());}
+  {auto check = [](selectableEntity & se){return se.name == "Important Title" && se.uid.to_string() == "{07e453ad-b698-47b8-aa52-c7ef2306731d}" && se.level == 1;};
+  REQUIRE(std::find_if(list.begin(), list.end(), check) != list.end());}
+
+  double dummy=0.0;
+  auto fte = sig.stashPayloadForReturn(dummy, dummy, false);
+  REQUIRE_THAT(fte.first, WithinAbs(0.75, margin));
+  REQUIRE_THAT(fte.second, WithinAbs(0.25, margin));
+
+}
+TEST_CASE("Known Data - Load projects with active project", "[QTAware]"){
+  auto app = dummyApp();
+  TrackerData td{basicConfig("./InputData/KnownDatabaseActive.db")};
+
+  SignalCatcher sig;
+  QAbstractEventDispatcher::connect(&td, &TrackerData::projectRunningUpdate, &sig, &SignalCatcher::emitString);
+
+  td.loadProjects(4000);
+
+  std::string str;
+  str = sig.stashPayloadForReturn(str, false);
+  REQUIRE(str == "Testing");
+
+}
 // ---------- Special functions ---------------------------------------------------------------------
 
 TEST_CASE("Known Data - Timestamps before", "[QTAware]"){

4bfe0b278a923653e7bb5a8f6be5d94d2ca74b07
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-03-21 12:31:21 +0000

    Method to get unique files
    Now using a LOT of scratch databases and names risk colliding. Use
    this for a unique name

--- tests/shorthand.h
+++ tests/shorthand.h
@@ -1,7 +1,15 @@
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <QUuid>
 
 #ifndef __shorthand
 #define __shorthand
 static const float margin = 0.001; // Float margin
 static auto inline WithinAbs = Catch::Matchers::WithinAbs;
+
+//As we're already using QUuid, use it for scratch file names too
+inline std::string getScratchFileName(std::string path = "./Scratch/"){
+    auto tmp = QUuid::createUuid().toString().toStdString();
+    return path + tmp.substr(1, tmp.size()-2) +".db"; // Strip the '{}' wrapper
+}
+
 #endif

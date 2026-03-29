56cceac319110066aed1ea23cbfc087a109ed976
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-03-29 15:50:48 +0100

    Special float
    Float to help solve the penny problem

--- include/support.h
+++ include/support.h
@@ -29,6 +29,46 @@ const bool NO_FORCE = false;
 const std::string appVersion = "0.2.0";
 const std::string appName = "Time Tracker Two";
 
+struct exactBoundedFloat{
+  static constexpr int permyriad = 10000; // Normalisation constant
+  static constexpr double halfinc = 0.5; // Float corresponding to half a tick
+  int value = 0;
+  exactBoundedFloat():value(0){;}
+  explicit exactBoundedFloat(int val){set(val);}
+  explicit operator float()const{return (double)(value)/(double)permyriad;}
+  void set(int value_in){
+    if(value_in >= 0 && value_in <= permyriad){
+      value = value_in;
+    }else{
+      throw std::runtime_error("Bad value in setter");
+    }
+  }
+  void set(double approx_in){
+    int val = std::floor((approx_in * permyriad)+halfinc); // NINT
+    set(val);
+  }
+};
+inline bool operator==(const exactBoundedFloat & a, const float & b){
+  return std::abs((float)a - b) < 1e-5;
+}
+inline std::ostream& operator<< (std::ostream& stream, const exactBoundedFloat & flt){
+  stream<<((float) flt);
+  return stream;
+}
+using eb_float = exactBoundedFloat;
+inline std::string exactPercent(exactBoundedFloat val){
+  if(val.value == 0) return "0.00"; //Easier to handle this case specially
+  int leading_digs = (val.value == 10000 ? 3 : (val.value > 999 ? 2 : 1)); //Display as 0.xyz
+  std::stringstream ss;
+  ss<<val.value;
+  std::string str = ss.str();
+  if(val.value < 100) str = '0'+str; //Restore leading zero
+  if(val.value < 10) str = '0'+str; //Another
+  //Insert decimal pt
+  str = str.substr(0, leading_digs)+'.'+str.substr(leading_digs, str.size());
+  return str;
+}
+
 /** \brief Time constants
 *
 * Factors for quick conversions from seconds etc. For important time values use timeWrapper
--- tests/test_supportCode.cpp
+++ tests/test_supportCode.cpp
@@ -25,3 +25,130 @@ TEST_CASE("Float formatting 0.25", "[Support]"){
     REQUIRE(displayFloatQuarters(-45.0) == "-45.0");
     REQUIRE(displayFloatQuarters(-4.56) == "-4.5");
 }
+
+TEST_CASE("Bounded float create", "[Support]"){
+
+  SECTION("Small"){
+    eb_float value{100};
+    REQUIRE( std::abs((float)value - 0.01) < 1e-5);
+  }
+  SECTION("Max"){
+    eb_float value{10000};
+    REQUIRE( std::abs((float)value - 1.0) < 1e-5);
+  }
+  SECTION("Mid"){
+    eb_float value{4731};
+    REQUIRE( std::abs((float)value - 0.4731) < 1e-5);
+  }
+}
+TEST_CASE("Bounded float set", "[Support]"){
+
+  eb_float value;
+  SECTION("Small"){
+    value.set(100);// 1% or 0.01
+    REQUIRE( std::abs((float)value - 0.01) < 1e-5);
+  }
+  SECTION("Max"){
+    value.set(10000);
+    REQUIRE( std::abs((float)value - 1.0) < 1e-5);
+  }
+  SECTION("Mid range"){
+    value.set(5320);
+    REQUIRE( std::abs((float)value - 0.532) < 1e-5);
+  }
+  SECTION("Zero"){
+    value.set(0);
+    REQUIRE( std::abs((float)value - 0.0) < 1e-5);
+  }
+}
+TEST_CASE("Bounded float stream", "[Support]"){
+  eb_float val;
+  std::stringstream ss;
+  SECTION("Midrange"){
+    val.set(7249);
+    ss<< val;
+    REQUIRE(ss.str() == "0.7249");
+  }
+  SECTION("Midrange - trailing 0"){
+    val.set(7240);
+    ss<<val;
+    REQUIRE(ss.str() == "0.724");
+  }
+  SECTION("Small"){
+    val.set(15);
+    ss<<val;
+    REQUIRE(ss.str() == "0.0015");
+  }
+  SECTION("Tiny"){
+    val.set(1);
+    ss<<val;
+    REQUIRE(ss.str() == "0.0001");
+  }
+}
+TEST_CASE("Bounded float display", "[Support]"){
+  eb_float val;
+  SECTION("Midrange"){
+    val.set(7249);
+    auto str = exactPercent(val);
+    REQUIRE(str == "72.49");
+  }
+  SECTION("Midrange - trailing 0"){
+    val.set(7240);
+    auto str = exactPercent(val);
+    REQUIRE(str == "72.40");
+  }
+  SECTION("Small"){
+    val.set(15);
+    auto str = exactPercent(val);
+    REQUIRE(str == "0.15");
+  }
+  SECTION("Tiny"){
+    val.set(1);
+    auto str = exactPercent(val);
+    REQUIRE(str == "0.01");
+  }
+  SECTION("Zero"){
+    val.set(0);
+    auto str = exactPercent(val);
+    REQUIRE(str == "0.00");
+  }
+  SECTION("Max"){
+    val.set(10000);
+    auto str = exactPercent(val);
+    REQUIRE(str == "100.00");
+  }
+}
+TEST_CASE("Bounded float out of bounds", "[Support]"){
+  eb_float value;
+  SECTION("Setters"){
+    REQUIRE_THROWS( value.set(-1));
+    REQUIRE_THROWS( value.set(10001));
+    REQUIRE_THROWS( value.set(50001));
+  }
+  SECTION("Bad construction"){
+    auto fn =[](int i){return eb_float{i};};
+    REQUIRE_THROWS(fn(-1));
+    REQUIRE_THROWS(fn(10001));
+    REQUIRE_THROWS(fn(500001));
+  }
+}
+TEST_CASE("Bounded float set and compare float", "[Support]"){
+  eb_float value;
+  SECTION("Small"){
+    value.set(0.01);// 1% or 0.01
+    std::cout<<(float) value<<std::endl;
+    REQUIRE(value == 0.01);
+  }
+  SECTION("Max"){
+    value.set(1.0);
+    REQUIRE(value == 1.0);
+  }
+  SECTION("Mid range"){
+    value.set(0.532);
+    REQUIRE(value == 0.532);
+  }
+  SECTION("Zero"){
+    value.set(0.0);
+    REQUIRE( value == 0.0);
+  }
+}

d4428717b73d80bb3206a640ba489d7ff72c071b
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-04-04 16:14:59 +0100

    Extend test data
    Adds another window, just to check more thoroughly

--- tests/test_ganttProcessor.cpp
+++ tests/test_ganttProcessor.cpp
@@ -19,11 +19,13 @@ TEST_CASE("Forming Envelope", "[Only]"){
   }
   SECTION("Fully specified"){
     auto id = proIds::Uuid("{2c531a42-d999-4c0f-b6fd-f9417e69e715}");
-    REQUIRE(entries[id].slices.size() == 2);
-    auto slice1 = singleSlice{125, 275, eb_float{1000}};
+    REQUIRE(entries[id].slices.size() == 3);
+    auto slice1 = singleSlice{125, 200, eb_float{1000}};
     REQUIRE(entries[id].slices[0] == slice1);
-    slice1 = singleSlice{275, e_end, eb_float{1500}};
+    slice1 = singleSlice{200, 275, eb_float{1200}};
     REQUIRE(entries[id].slices[1] == slice1);
+    slice1 = singleSlice{275, e_end, eb_float{1500}};
+    REQUIRE(entries[id].slices[2] == slice1);
   }
   SECTION("No end specified"){
     auto id = proIds::Uuid("{8af5d44a-2921-4666-b33b-053459e2ced6}");
@@ -47,10 +49,10 @@ TEST_CASE("Reprocessing case", "[Only]"){
   entries = ganttProcessor::reprocess(entries);
 
   REQUIRE(entries.size() == 4);
-  std::vector<timecode> s_edges{50, 125, 275, 300}; // Expected common bin edges
+  std::vector<timecode> s_edges{50, 125, 200, 275, 300}; // Expected common bin edges
   SECTION("Unspecified ends"){
     auto id = proIds::Uuid("{cc467402-acd5-494f-9c58-466f3aa6f117}");
-    REQUIRE(entries[id].slices.size() == 3);
+    REQUIRE(entries[id].slices.size() == 4);
     //Same FTE, but now 3 bins
     for(size_t i = 0; i< 3; i++){
       auto slice1 = singleSlice{s_edges[i], s_edges[i+1], eb_float{5000}};
@@ -59,15 +61,17 @@ TEST_CASE("Reprocessing case", "[Only]"){
   }
   SECTION("Fully specified"){
     auto id = proIds::Uuid("{2c531a42-d999-4c0f-b6fd-f9417e69e715}");
-    REQUIRE(entries[id].slices.size() == 2);
-    auto slice1 = singleSlice{125, 275, eb_float{1000}};
+    REQUIRE(entries[id].slices.size() == 3);
+    auto slice1 = singleSlice{125, 200, eb_float{1000}};
     REQUIRE(entries[id].slices[0] == slice1);
-    slice1 = singleSlice{275, 300, eb_float{1500}};
+    slice1 = singleSlice{200, 275, eb_float{1200}};
     REQUIRE(entries[id].slices[1] == slice1);
+    slice1 = singleSlice{275, 300, eb_float{1500}};
+    REQUIRE(entries[id].slices[2] == slice1);
   }
   SECTION("No end specified"){
     auto id = proIds::Uuid("{8af5d44a-2921-4666-b33b-053459e2ced6}");
-    REQUIRE(entries[id].slices.size() == 3);
+    REQUIRE(entries[id].slices.size() == 4);
     //Same FTE, but now 3 bins
     for(size_t i = 0; i< 3; i++){
       auto slice1 = singleSlice{s_edges[i], s_edges[i+1], eb_float{2200}};
@@ -76,7 +80,7 @@ TEST_CASE("Reprocessing case", "[Only]"){
   }
   SECTION("No start specifed"){
     auto id = proIds::Uuid("{7228d8fe-0782-4205-9ed3-dca2693c0d1f}");
-    REQUIRE(entries[id].slices.size() == 3);
+    REQUIRE(entries[id].slices.size() == 4);
     //Same FTE, but now 3 bins
     for(size_t i = 0; i< 3; i++){
       auto slice1 = singleSlice{s_edges[i], s_edges[i+1], eb_float{100}};
